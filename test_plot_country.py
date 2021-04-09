9c536172e3a3d94c78544a655f641e75fcd27eec
Arnav Gupta <arnavgupta19@gmail.com>
2021-04-09 01:41:55 -0400

    Deleted extraneous files, finalized pylint for test files. Small changes to compessay

--- test_plot_country.py
+++ test_plot_country.py
@@ -1,26 +1,23 @@
-fromp plot_country import(
-    # makePlots,
-    read_cSV,
-    list_to_string,
-    csv_name,
-    # rollingAverage,
-    # histograms,
-    # boxplots
-    
-    
-)
-from collections import Counter
+"""
+docstring
+"""
 import pytest
-from pytrends.request import TrendReq
 import pandas as pd
+from plot_country import(
+    read_csv,
+    csv_name,
+    list_to_string,
+)
 
-# readCSV preparation
-kw_list = ["Tunisia Democracy"]  
-data = pd.read_csv('pytrends_Data/'+csvName(kw_list)+'.csv')
+# read_csv preparation solutions
+kw_list = ["Tunisia Democracy"]
+data = pd.read_csv('pytrends_data/'+csv_name(kw_list)+'.csv')
 
-readCSV_cases = [(kw_list, data)]
 # Define sets of test cases
-listToString_cases = [
+read_csv_cases = [(kw_list, data)]
+
+# Define sets of test cases
+list_to_string_cases = [
     # Check that 1 length list is returned as string without commas
     (["Country 1"], 'Country 1'),
     # Check two countries are returned with a comma and space
@@ -35,13 +32,14 @@ listToString_cases = [
      'Country 1, Country 2, Country 3, Country 4, Country 5')
 ]
 
-csvName_cases = [
+# Define sets of test cases
+csv_name_cases = [
     # Check that 1 length list is returned as string without spaces
     (["Country 1"], 'Country1'),
     # Check two countries are returned with no spaces, '_' between indexes
     (["Country 1", "Country 2"], 'Country1_Country2'),
     # Check three countries are returned with no spaces, '_' between indexes
-    (["Country 1", "Country 2", "Country 3"], 
+    (["Country 1", "Country 2", "Country 3"],
      'Country1_Country2_Country3'),
     # Check four countries are returned with no spaces, '_' between indexes
     (["Country 1", "Country 2", "Country 3", "Country 4"],
@@ -51,19 +49,23 @@ csvName_cases = [
      'Country1_Country2_Country3_Country4_Country5')
 ]
 
-@pytest.mark.parametrize("countryList, countryString", listToString_cases)
-def test_listToString(countryList, countryString):
-    assert listToString(countryList) == countryString
-    
-@pytest.mark.parametrize("countryList, countryString", csvName_cases)
-def test_csvName(countryList, countryString):
-    assert csvName(countryList) == countryString
-@pytest.mark.parametrize("countryList, countryString", csvName_cases)
-def test_csvName(countryList, countryString):
-    assert csvName(countryList) == countryString
-@pytest.mark.parametrize("functionData, data", readCSV_cases)
-def test_csvName(functionData, data):
-    # Receiving data from google to test readCSV function once. Done so to not ping
-    # Google constantly. No other way to construct the dataframe other than using
-    # the readCSV function, which would defeat the purpose of the test. 
-    assert readCSV(functionData).equals(data)
+@pytest.mark.parametrize("country_list, country_string", list_to_string_cases)
+def test_list_to_string(country_list, country_string):
+    """
+    docstring
+    """
+    assert list_to_string(country_list) == country_string
+@pytest.mark.parametrize("country_list, country_string", csv_name_cases)
+def test_csv_name(country_list, country_string):
+    """
+    docstring
+    """
+    assert csv_name(country_list) == country_string
+@pytest.mark.parametrize("function_data, data_sol", read_csv_cases)
+def test_read_csv_cases(function_data, data_sol):
+    """
+    Receiving data from google to test read_csv function once. Done so to not ping
+    Google constantly. No other way to construct the dataframe other than using
+    the read_csv function, which would defeat the purpose of the test.
+    """
+    assert read_csv(function_data).equals(data_sol)
